8397a1be451d7b9d05731ee189a5c38b4b216a48
VladislavTolstikov <omfgzwero@gmail.com>
2022-04-01 14:58:18 +0300

    add welcome user 100% working

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import brain_games.cli
 
 def main():
 	print("Welcome to Brain Games!")
+	brain_games.welcome_user()
+	
 
 if __name__ == '__main__':
 	main()
