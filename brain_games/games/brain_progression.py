181834a3d05a7769bc275992fb7a22b38aaa0fa2
VladislavTolstikov <omfgzwero@gmail.com>
2022-05-17 15:47:01 +0300

    fix progression

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -1,27 +1,32 @@
-"""Game with progreesion."""
+"""Game with progression."""
 
 
 from random import randint
 
 
-DESCRIPTION = 'WWhat number is missing in the progression?'
+DESCRIPTION = 'What number is missing in the progression?'
+
+
+def fill_progression():
+    first = 0
+    plus = 0
+    first = randint(1, 1000)
+    plus = randint(1, 25)
+    progression = [0 for x in range(0, 10)]
+    progression[0] = first
+    for index in range(1, 10):
+        progression[index] = progression[index - 1] + plus
+    return progression
 
 
 def generate_question():
     """Define progression game logic."""
-    first = 0
-    plus = 0
     blank_index = 0
     question_string = ''
     answer = 0
-    first = randint(1, 1000)
-    plus = randint(1, 25)
     blank_index = randint(0, 9)
-    prog = [0 for x in range(0, 10)]
-    prog[0] = first
-    for index in range(1, 10):
-        prog[index] = prog[index - 1] + plus
-    answer = prog[blank_index]
-    prog[blank_index] = '..'
-    question_string = ' '.join(str(e) for e in prog)
+    progression = fill_progression()
+    answer = progression[blank_index]
+    progression[blank_index] = '..'
+    question_string = ' '.join(str(e) for e in progression)
     return (str(answer), question_string)
