969ad15ce246e7a43395399ede70c7984f396479
VladislavTolstikov <omfgzwero@gmail.com>
2022-05-17 15:27:27 +0300

    def new prime function

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -7,9 +7,7 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def generate_question():
-    """Define prime game main logic."""
-    question = randint(4, 501)
+def check_prime(question):
     flag = 0
     start = 2
     while start ** 2 <= question and flag != 1:
@@ -17,5 +15,12 @@ def generate_question():
             flag = 1
         start += 1
     if flag == 1:
-        return ['no', question]
-    return ['yes', question]
+        return 'no'
+    return 'yes'
+
+
+def generate_question():
+    """Define prime game main logic."""
+    question = randint(4, 501)
+    answer = check_prime(question)
+    return (answer, question)
