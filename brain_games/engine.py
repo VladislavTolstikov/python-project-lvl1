bcb2d6fcb505444908f6327ffd2dde07b52d4524
VladislavTolstikov <omfgzwero@gmail.com>
2022-05-31 22:13:44 +0300

    Use for in engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -11,15 +11,13 @@ def play(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
-    counter = 0
-    while counter < TRIES:
+    for counter in range(0,TRIES):
         correct_answer, question = game.generate_question()
         print(game.DESCRIPTION)
-        print('Question: {0}'.format(question))
+        print(f"Question: {question}")
         user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
             print('Correct!')
-            counter += 1
         else:
             print("'{0}' is wrong answer ;(. Correct answer was"
                   "'{1}'.".format(user_answer, correct_answer)
