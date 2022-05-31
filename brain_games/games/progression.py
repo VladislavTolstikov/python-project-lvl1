d758fb9d6deebd5af1ec303a32ca0cfbf5d3ded1
VladislavTolstikov <omfgzwero@gmail.com>
2022-05-31 21:59:19 +0300

    Use append in progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,25 +11,20 @@ PROGRESSION_SIZE = 10
 
 
 def generate_progression():
-    first_element = 0
-    step = 0
-    first_element = str(randint(1, 1000))
+    first_element = randint(1, 1000)
     step = randint(1, 25)
-    progression = ['' for x in range(0, PROGRESSION_SIZE)]
-    progression[0] = first_element
+    progression = []
+    progression.append(first_element)
     for index in range(1, PROGRESSION_SIZE):
-        progression[index] = int(progression[index - 1]) + step
-        progression[index] = str(progression[index])
+        progression.append(progression[index - 1] + step)
     return progression
 
 
 def generate_question():
     """Define progression game logic."""
-    blank_index = 0
-    question_string = ''
     answer = 0
     blank_index = randint(0, PROGRESSION_SIZE - 1)
-    progression = generate_progression()
+    progression = list(map(str, generate_progression()))
     answer = progression[blank_index]
     progression[blank_index] = '..'
     question_string = ' '.join(str(e) for e in progression)
