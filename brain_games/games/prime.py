2d38c8fa18e0d963eb73ccb5dc8858b0d2b9fc3e
VladislavTolstikov <omfgzwero@gmail.com>
2022-06-01 15:25:40 +0300

    Fix check of prime and return in prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,13 +7,12 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def check_prime(question):
-    if question <= 1:
+def is_prime(number):
+    if number <= 1:
         return False
-    flag = 0
     start = 2
-    while start ** 2 <= question and flag != 1:
-        if question % start == 0:
+    while start ** 2 <= number:
+        if number % start == 0:
             return False
         start += 1
     return True
@@ -22,7 +21,5 @@ def check_prime(question):
 def generate_question():
     """Define prime game main logic."""
     question = randint(-100, 501)
-    if check_prime(question) is True:
-        return ('yes', question)
-    else:
-        return('no', question)
+    answer = 'yes' if is_prime(question) else 'no'
+    return (answer, question)
